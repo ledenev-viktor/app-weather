336e460edd7b50d2f05e005288c3cac64cde2a1f
ledenev-viktor <ledenev4252@gmail.com>
2023-08-13 13:20:54 +0300

    скрипт работы cli пакета погоды

---
+++ weather.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+import { getArgs } from "./helpers/args.js";
+import { getIcon, getWeather } from "./services/api.service.js";
+import {
+  printError,
+  printHelp,
+  printSuccess,
+  printWeather,
+} from "./services/log.service.js";
+import {
+  TOKEN_DICTIONARY,
+  getKeyValue,
+  saveKeyValue,
+} from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+  if (!token.length) {
+    printError("Не передан token");
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
+    printSuccess("Token сохранен");
+  } catch (error) {
+    printError(error.message);
+  }
+};
+
+const saveCity = async (city) => {
+  if (!city.length) {
+    printError("Не передан город");
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.city, city);
+    printSuccess("Город сохранен");
+  } catch (error) {
+    printError(error.message);
+  }
+};
+
+const getForcast = async () => {
+  try {
+    const city = process.env.CITY ?? (await getKeyValue(TOKEN_DICTIONARY.city));
+    const weather = await getWeather(city);
+    printWeather(weather, getIcon(weather.weather[0].icon));
+  } catch (error) {
+    if (error?.response?.status === 404) {
+      printError("Неверно указан город");
+    } else if (error?.response?.status === 401) {
+      printError("Неверно указан token");
+    } else {
+      printError(error.message);
+    }
+  }
+};
+
+const initCLI = () => {
+  const args = getArgs(process.argv);
+
+  if (args.h) {
+    return printHelp();
+  }
+
+  if (args.s) {
+    return saveCity(args.s);
+  }
+
+  if (args.t) {
+    return saveToken(args.t);
+  }
+
+  return getForcast();
+};
+initCLI();
